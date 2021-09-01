78f363f02aaf2ba672ae82c4378dec79ca9b25eb
chengweixuan <exweixuan@gmail.com>
2021-09-02 01:33:16 +0800

    initial commit

---
+++ src/main/java/com/weixuan/ocbcinemabackend/OcbcinemaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.weixuan.ocbcinemabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcbcinemaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcbcinemaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/weixuan/ocbcinemabackend/OcbcinemaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weixuan.ocbcinemabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcbcinemaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
